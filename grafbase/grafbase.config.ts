7b93ce675787bce466bc623fc2ccb9a81df14c87
Andrew Schuessler <arschuessler90@gmail.com>
2023-07-27 22:39:37 -0700

    1st night commit.

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -4,7 +4,7 @@ const User = g.model("User", {
   name: g.string().length({ min: 2, max: 20 }),
   email: g.string().unique(),
   avatarUrl: g.url(),
-  description: g.sring().optional(),
+  description: g.string().optional(),
   githubUrl: g.url().optional(),
   linkedInUrl: g.url().optional(),
   projects: g
